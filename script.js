2d3300259a13390318a79a3c36ffa91969afc571
James <jamesd-glastonbury@thecoderschool.com>
2025-04-25 16:11:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ var memes = [
   "https://i.imgflip.com/9nm0tk.jpg",
   "https://i.pinimg.com/474x/af/44/ca/af44cac38b38e9c5e9a8dad7549d9e65.jpg",
   "https://i.imgflip.com/9r9135.jpg",
+  "https://i.imgflip.com/9rpd0e.jpg",
 ]
 var topimage = document.getElementById("ahhh");
 
