73f0a55085312e11d1db9b3a276502b3b7ca3ab7
James <jamesd-glastonbury@thecoderschool.com>
2026-04-01 17:21:10 -0400

    Add new image URL to the image array

--- script.js
+++ script.js
@@ -53,6 +53,7 @@ var memes = [
   "https://i.imgflip.com/9tc1hw.jpg",
   "https://i.imgflip.com/ao5ldx.jpg",
   "https://i.imgflip.com/9fijd3.jpg",
+  "https://i.imgflip.com/a27tzc.jpg",
 ]
 var topimage = document.getElementById("ahhh");
 var rick = document.getElementById("rickroll");
